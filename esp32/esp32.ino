d7e1c58c390e997456f8a9c94168e1a9ea8e4fa3
Renan Santana <renan_ota@hotmail.com>
2022-04-20 01:05:46 -0300

    finish web server

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -5,9 +5,11 @@
 
 const int lcdColumns = 16;
 const int lcdRows = 2;
+String message = "";
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 WebServer server(80);
+TaskHandle_t scrollTextHandle;
 
 
 void setupLcd() {
@@ -15,25 +17,40 @@ void setupLcd() {
   lcd.backlight();
 }
 
+void scrollText(void *arg) {
+  const int delayTime = 300;
+  for (int i=0; i < lcdColumns; i++) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+  for (int pos = 0; pos < message.length(); pos++) {
+    lcd.setCursor(0, 1);
+    lcd.print(message.substring(pos, pos + lcdColumns));
+    delay(delayTime);
+  }
+  scrollText(NULL);
+  // vTaskDelete(NULL);
+}
+
 void setupWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     lcd.setCursor(0, 0);
-    lcd.print("Connecting to WiFi:");
+    lcd.print("Connecting to:");
     lcd.setCursor(0, 1);
     lcd.print(WIFI_SSID);
   }
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Connected to the WiFi network");
+  lcd.print("Connected!");
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP());
 }
 
 void setupServer() {
   server.on("/", handle_OnConnect);
-  server.on("/notification", handle_notification);
+  server.on("/post", HTTP_POST, handle_notification);
   server.onNotFound(handle_NotFound);
   server.begin();
 }
@@ -43,7 +60,33 @@ void handle_OnConnect() {
 }
 
 void handle_notification() {
-
+  if (server.hasArg("notification")) {
+    server.send(200, "text/html", "Received");
+    message = server.arg("notification");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Notification:");
+    if (message.length() > lcdColumns) {
+      if (scrollTextHandle != NULL) {
+        vTaskDelete(scrollTextHandle);
+        scrollTextHandle = NULL;
+      }
+      xTaskCreatePinnedToCore(
+        scrollText,         // function to run
+        "scrollText",       // task name
+        10000,              // stack size (bytes)
+        NULL,               // params
+        0,                  // priority 
+        &scrollTextHandle,  // task handle
+        1                   // core
+      );
+    } else {
+      lcd.setCursor(0, 1);
+      lcd.print(message);
+    }
+  } else {
+    server.send(422, "text/html", "Missing 'notification'");
+  }
 }
 
 void handle_NotFound(){
